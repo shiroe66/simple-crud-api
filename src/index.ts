c430ea2805e145012dce51394f5a65c5f96636de
shiroe66 <shiroeqky@gmail.com>
2022-06-17 22:47:28 +0500

    refactor: added controller n model users

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-import { createServer } from "http"
 import "dotenv/config"
-import { onGetMethod } from "./config/onGetMethod"
+import { createServer } from "http"
+import { getUser, getUsers } from "./controller/usersController"
 import { parseURL } from "./helpers/parseURL"
-import { isValidId } from "./helpers/isValidId"
 
 const server = createServer((req, res) => {
-  res.setHeader("Content-type", "application/json")
-
   const url = parseURL(req.url as string)
-  const id = isValidId(url.slice(11))
 
-  switch (req.method) {
-    case "GET":
-      onGetMethod(url, res)
-      break
-    case "POST":
-      break
-    case "PUT":
-      break
-    case "DELETE":
-      break
+  if (url === "/api/users" && req.method === "GET") {
+    getUsers(req, res)
+  } else if (
+    url.match(new RegExp(/^\/api\/users\/[\w-]+$/)) &&
+    req.method === "GET"
+  ) {
+    const id = url.split("/").pop() as string
+    getUser(req, res, id)
+  } else {
+    res.writeHead(404, { "Content-type": "application/json" })
+    res.end(JSON.stringify({ message: "Route not found" }))
   }
 })
 
