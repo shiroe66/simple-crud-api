7d5c16907160e9dfcabeff470988c219a7b27b64
rasul <shiroeqky@gmail.com>
2022-06-18 19:22:55 +0500

    feat: POST user function

--- src/controller/usersController.ts
+++ src/controller/usersController.ts
@@ -1,31 +1,58 @@
-import { IncomingMessage, ServerResponse } from "http"
-import { find, findAll } from "../models/usersModel"
+import { IncomingMessage, ServerResponse } from 'http'
+import { add, find, findAll } from '../models/usersModel'
+import { User } from '../types/user.types'
 
 export const getUsers = async (req: IncomingMessage, res: ServerResponse) => {
   try {
     const users = await findAll()
 
-    res.writeHead(200, { "Content-type": "application/json" })
+    res.writeHead(200, { 'Content-type': 'application/json' })
     res.end(JSON.stringify(users))
   } catch (error) {
     console.error(error)
   }
 }
 
-export const getUser = async (
-  req: IncomingMessage,
-  res: ServerResponse,
-  id: string
-) => {
+export const getUser = async (req: IncomingMessage, res: ServerResponse, id: string) => {
   try {
     const user = await find(id)
 
     if (user) {
-      res.writeHead(200, { "Content-type": "application/json" })
+      res.writeHead(200, { 'Content-type': 'application/json' })
       res.end(JSON.stringify(user))
     } else {
-      res.writeHead(404, { "Content-type": "application/json" })
-      res.end(JSON.stringify({ message: "User not found" }))
+      res.writeHead(404, { 'Content-type': 'application/json' })
+      res.end(JSON.stringify({ message: 'User not found' }))
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const addUser = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const buffers: Uint8Array[] = []
+
+    for await (const chunk of req) {
+      buffers.push(chunk)
+    }
+
+    const data = Buffer.concat(buffers).toString()
+    const { username, age, hobbies } = JSON.parse(data)
+
+    if (username && age && hobbies) {
+      const user: User = {
+        username,
+        age,
+        hobbies,
+      }
+
+      const newUser = await add(user)
+      res.writeHead(201, { 'Content-type': 'application/json' })
+      res.end(JSON.stringify(newUser))
+    } else {
+      res.writeHead(400, { 'Content-type': 'application/json' })
+      res.end(JSON.stringify({ message: "Body doesn't contain requirement fields" }))
     }
   } catch (error) {
     console.error(error)
--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -1,4 +1,6 @@
-import { users } from "../helpers/users"
+import { users } from '../helpers/users'
+import { User } from '../types/user.types'
+import { v4 as uuidv4 } from 'uuid'
 
 export const findAll = () => {
   return new Promise((res, rej) => res(users))
@@ -8,5 +10,16 @@ export const find = (id: string) => {
   return new Promise((res, rej) => {
     const user = users.find((user) => user.id === id)
     res(user)
+  })
+}
+
+export const add = (user: User) => {
+  return new Promise((res, rej) => {
+    const newUser = {
+      id: uuidv4(),
+      ...user,
+    }
+    users.push(newUser)
+    res(newUser)
   })
 }
