fa4ba424b8c4cbad22774ab8b90dbbac52fd9061
shiroe66 <shiroeqky@gmail.com>
2022-06-19 21:37:54 +0500

    fix: regexp

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,57 @@
-import 'dotenv/config'
-import { createServer } from 'http'
-import { getUser, getUsers, addUser, updateUser, deleteUser } from './controller/usersController'
-import { isValidId } from './helpers/isValidId'
-import { parseURL } from './helpers/parseURL'
+import "dotenv/config"
+import { createServer } from "http"
+import {
+  getUser,
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+} from "./controller/usersController"
+import { isValidId } from "./helpers/isValidId"
+import { parseURL } from "./helpers/parseURL"
 
 const server = createServer((req, res) => {
   try {
     const url = parseURL(req.url as string)
 
-    if (url === '/api/users' && req.method === 'GET') {
+    if (url === "/api/users" && req.method === "GET") {
       getUsers(req, res)
-    } else if (url.match(new RegExp(/^\/api\/users\/[\w-]+$/)) && req.method === 'GET') {
-      const id = url.split('/').pop() as string
+    } else if (url.match(/^\/api\/users\/[\w-]+$/) && req.method === "GET") {
+      const id = url.split("/").pop() as string
 
       if (isValidId(id)) {
         getUser(req, res, id)
       } else {
-        res.writeHead(400, { 'Content-type': 'application/json' })
-        res.end(JSON.stringify({ message: 'ID is not valid' }))
+        res.writeHead(400, { "Content-type": "application/json" })
+        res.end(JSON.stringify({ message: "ID is not valid" }))
       }
-    } else if (url === '/api/users' && req.method === 'POST') {
+    } else if (url === "/api/users" && req.method === "POST") {
       addUser(req, res)
-    } else if (url.match(new RegExp(/^\/api\/users\/[\w-]+$/)) && req.method === 'PUT') {
-      const id = url.split('/').pop() as string
+    } else if (url.match(/^\/api\/users\/[\w-]+$/) && req.method === "PUT") {
+      const id = url.split("/").pop() as string
 
       if (isValidId(id)) {
         updateUser(req, res, id)
       } else {
-        res.writeHead(400, { 'Content-type': 'application/json' })
-        res.end(JSON.stringify({ message: 'ID is not valid' }))
+        res.writeHead(400, { "Content-type": "application/json" })
+        res.end(JSON.stringify({ message: "ID is not valid" }))
       }
-    } else if (url.match(new RegExp(/^\/api\/users\/[\w-]+$/)) && req.method === 'DELETE') {
-      const id = url.split('/').pop() as string
+    } else if (url.match(/^\/api\/users\/[\w-]+$/) && req.method === "DELETE") {
+      const id = url.split("/").pop() as string
 
       if (isValidId(id)) {
         deleteUser(req, res, id)
       } else {
-        res.writeHead(400, { 'Content-type': 'application/json' })
-        res.end(JSON.stringify({ message: 'ID is not valid' }))
+        res.writeHead(400, { "Content-type": "application/json" })
+        res.end(JSON.stringify({ message: "ID is not valid" }))
       }
     } else {
-      res.writeHead(404, { 'Content-type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Route not found' }))
+      res.writeHead(404, { "Content-type": "application/json" })
+      res.end(JSON.stringify({ message: "Route not found" }))
     }
   } catch (error) {
-    res.writeHead(500, { 'Content-type': 'application/json' })
-    res.end(JSON.stringify({ message: 'Internal server error' }))
+    res.writeHead(500, { "Content-type": "application/json" })
+    res.end(JSON.stringify({ message: "Internal server error" }))
   }
 })
 
